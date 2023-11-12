3520c74724a677f5dc6debcf0808d1fd097c9c08
SoongE <oh1.seungmin@gmail.com>
2023-11-12 18:58:59 +0900

    Clean code

--- src/models/utils.py
+++ src/models/utils.py
@@ -1,6 +1,5 @@
 def activation_for_substitute(xs, x):
     dead_idx = x == 0
-    dead_idx.unsqueeze(-1).repeat(1, 1, 1, 1, xs.size(-1))
     xs[dead_idx] = 0
     return xs
 
@@ -9,5 +8,3 @@ def deploy(model):
     for name, module in model.named_modules():
         if hasattr(module, 're_parameterize'):
             module.re_parameterize()
-    # if hasattr(model.fc, 're_parameterize'):
-    #     model.fc.re_parameterize()
